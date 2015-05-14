088f3fac2b050a4b40eec9aa2479427c8ca0c61f
Michael Aquilina <michaelaquilina@gmail.com>
2015-05-14 22:50:16 +0100

    Add download and setup pycharm step

---
+++ roles/gnome/files/setup_pycharm.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+# Downloads a specific version of pycharm and installs it to the expected location
+mkdir -p /opt/jetbrains/pycharm
+
+wget http://download.jetbrains.com/python/pycharm-professional-3.4.1.tar.gz -O pycharm.tar.gz
+
+tar -xzf pycharm.tar.gz -C /opt/jetbrains/pycharm
+rm pycharm.tar.gz
+
