405a83ffbaecb82a690938edc9a733d20d6dcf05
Michael Aquilina <michaelaquilina@gmail.com>
2015-06-10 21:39:16 +0100

    Remove invalid synapse setup

--- roles/unity/files/setup_pycharm.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-# Downloads a specific version of pycharm and installs it to the expected location
-mkdir -p /opt/jetbrains/pycharm
-
-wget https://download.jetbrains.com/python/pycharm-professional-4.5.1.tar.gz -O pycharm.tar.gz
-
-tar -xzf pycharm.tar.gz -C /opt/jetbrains/pycharm
-rm pycharm.tar.gz
