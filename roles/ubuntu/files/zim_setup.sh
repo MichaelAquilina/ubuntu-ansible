61d4a84f5ae670452d157738609be26786f9abc0
Michael Aquilina <michaelaquilina@gmail.com>
2016-01-08 13:55:13 +0000

    Add zim setup and change to zim

---
+++ roles/ubuntu/files/zim_setup.sh
@@ -0,0 +1,8 @@
+#! /bin/zsh
+setopt EXTENDED_GLOB
+for template_file ( ${ZDOTDIR:-${HOME}}/.zim/templates/* ); do
+  user_file="${ZDOTDIR:-${HOME}}/.${template_file:t}"
+  ( print -n "$(<${template_file})\n$(<${user_file})" >! ${user_file} ) 2>/dev/null
+done
+
+source ${ZDOTDIR:-${HOME}}/.zlogin
