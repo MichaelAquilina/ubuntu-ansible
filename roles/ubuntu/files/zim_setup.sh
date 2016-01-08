503498302f2cb80fc1982f16110d14745dc4e976
Michael Aquilina <michaelaquilina@gmail.com>
2016-01-08 21:27:32 +0000

    Notify Zim Setup on change and set to executable permissions

--- roles/ubuntu/files/zim_setup.sh
+++ roles/ubuntu/files/zim_setup.sh
@@ -4,5 +4,3 @@ for template_file ( ${ZDOTDIR:-${HOME}}/.zim/templates/* ); do
   user_file="${ZDOTDIR:-${HOME}}/.${template_file:t}"
   ( print -n "$(<${template_file})\n$(<${user_file})" >! ${user_file} ) 2>/dev/null
 done
-
-source ${ZDOTDIR:-${HOME}}/.zlogin
