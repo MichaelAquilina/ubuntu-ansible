46a5abcbb95e66783d9be9514c6dfdf07b89839a
Michael Aquilina <michaelaquilina@gmail.com>
2016-02-05 13:51:43 +0000

    Add retrospective generator Tomboy shortcut

---
+++ roles/gnome/files/tomboy_retrospective.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+title="Retrospective $(date +"%U -%b")"
+
+# Opens a tomboy note with today's date as a title or creates it if it doesnt exist
+grep "<title>$title</title>" -r ~/.local/share/tomboy -h
+echo $?
+if [ $? -eq 0 ];
+then
+  tomboy --new-note "$title"
+else
+  tomboy --open-note "$title"
+fi
