8d456c4e35b2ae9c2e2846aa5e7e3128251c2ed3
Michael Aquilina <michaelaquilina@gmail.com>
2016-06-30 22:15:07 +0100

    Convert atom_init to a standard javascript file

---
+++ roles/gnome/files/atom_init.js
@@ -0,0 +1,33 @@
+document.body.classList.add("an-old-hope-modify-ui");
+
+atom.commands.add("atom-workspace", "dot-atom:to-snake-case", function() {
+  console.log("converting to snake case! (From Camel Case)");
+  let editor = atom.workspace.getActiveTextEditor();
+  let text = editor.getSelectedText();
+  console.log(text);
+
+  let output = [];
+  tokens = text.split(/(?=[A-Z])/);
+
+  tokens.forEach(t => {
+    output.push(t.toLowerCase());
+  });
+
+  editor.insertText(output.join("_"));
+})
+
+
+// Use full file path as Window title
+set_window_title = function() {
+  let window = atom.getCurrentWindow();
+  let editor = atom.workspace.getActiveTextEditor();
+  if (editor) {
+    relative_path = atom.project.relativizePath(editor.getPath())[1];
+    window.setTitle(`Atom - ${relative_path}`);
+  } else {
+    window.setTitle("Atom - None");
+  }
+}
+
+atom.workspace.onDidChangeActivePaneItem(set_window_title);
+set_window_title();
